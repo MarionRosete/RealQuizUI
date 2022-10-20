d3227ad1dc803d711728708086a893b88eb50f6c
MarionRosete <marion.rosete@fligno.com>
2022-10-20 22:25:20 +0800

    UI plotting

---
+++ src/page/root/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import quotes from '../../assets/quotes.json'
+
+const Root = () => {
+  return (
+    <div >
+      An A+ score is an indicator that someone had understood very well the subject matter.
+      <div>
+      We simple believe in fair and transparent quiz. That's All!
+      </div>
+       
+    </div>
+  )
+}
+
+export default Root
