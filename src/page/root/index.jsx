99e82c65d7ba9d0030c7b7f7caf99194a23772da
MarionRosete <marion.rosete@fligno.com>
2022-10-28 09:37:52 +0800

    1: init intro page

--- src/page/root/index.jsx
+++ src/page/root/index.jsx
@@ -4,11 +4,24 @@ import quotes from '../../assets/quotes.json'
 const Root = () => {
   return (
     <div >
-      An A+ score is an indicator that someone had understood very well the subject matter.
-      <div>
-      We simple believe in fair and transparent quiz. That's All!
+      <div className='text-4xl'>
+        An Online quiz platform with Transparency
       </div>
-       
+      <div className='mt-3'>
+        An A+ score is an indicator that someone had understood
+         very well the subject matter,
+         or more likely cheated a way.
+        <br/>
+        We simply believe in fair and transparent quiz. That's All!
+      </div >
+      <div className='mt-8'>
+        Be one of the quiz maker or be the quiz taker. you decide. 
+      </div>
+      <div className='p-2'>
+        <button  className='p-2 '>
+          Join us now
+        </button>
+      </div>
     </div>
   )
 }
