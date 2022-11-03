603f82e74ddb5327448b792d94127d30e01f1aad
MarionRosete <marion.rosete@fligno.com>
2022-11-03 16:32:03 +0800

    #1: base url

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const baseUrl = 'http://localhost:8000';
+const baseUrl = 'http://127.0.0.1:8000/api';
 const apiService = Axios.create();
 
 const withToken = (config) => {
