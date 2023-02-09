1b12e85e1c8a0ebc0141abdae763788aa74158ce
MarionRosete <marionrosete1@gmail.com>
2023-02-10 02:57:23 +0800

    #3: Create q and a UI. TBD fixed position and scrollable

---
+++ src/component/elements/RadioButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const RadioButton = ({value, onChange, name}) => {
+  return (
+    <input
+      type="radio"
+      value={value}
+      onChange={onChange}
+      name={name}
+      className="
+            w-4
+            h-4
+            text-blue-600
+            bg-gray-100
+            border-gray-300
+            focus:ring-blue-500
+            dark:focus:ring-blue-600
+            dark:ring-offset-gray-800
+            dark:bg-gray-700
+            dark:border-gray-600"
+    />
+  );
+};
+
+RadioButton.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func,
+  name: PropTypes.number,
+};
+export default RadioButton;
--- src/page/dashboard/CreateQandA.jsx
+++ src/page/dashboard/CreateQandA.jsx
@@ -1,8 +1,110 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TextArea from '../../component/elements/TextArea';
+import RadioButton from '../../component/elements/RadioButton';
 
+const initData= {
+  question: '',
+  choiceA: null,
+  choiceB: null,
+  choiceC: null,
+  choiceD: null,
+  answer: null,
+};
 const CreateQandA = () => {
+  const [qanda, setQandA] = useState([initData]);
+
+  const handleQuestion = (e, key) => {
+    const arr = [...qanda];
+    arr[key] = {...qanda[key], question: e.target.value};
+    setQandA(arr);
+  };
+
+  const handleAnswer = (e, key) => {
+    const arr = [...qanda];
+    arr[key] = {...qanda[key], answer: e.target.value};
+    setQandA(arr);
+  };
+
+  const handleChoice = (e, key, choice) => {
+    const arr = [...qanda];
+    arr[key] = {...qanda[key], [choice]: e.target.value};
+    setQandA(arr);
+  };
+
+  const handleAddItem = () => {
+    setQandA([...qanda, initData]);
+  };
+  console.log(qanda);
   return (
-    <div>CreateQandA</div>
+    <div className='mt-10'>
+      <div className='mt-5 mb-5'>
+        <button onClick={handleAddItem}>
+          Add item
+        </button>
+      </div>
+      <div className='space-y-10'>
+        {qanda.map((data, key)=>
+          <div key={key} className='space-y-3'>
+            <div className='flex gap-x-3 items-center'>
+              {key+1}.
+              <TextArea
+                placeholder={'Question'}
+                rows={'2'}
+                onChange={(e)=>handleQuestion(e, key)}
+              />
+            </div>
+            <div className='flex gap-x-3 items-center'>
+              <RadioButton
+                value={1}
+                onChange={(e)=>handleAnswer(e, key)}
+                name={key}
+              />
+              <TextArea
+                placeholder={'Choice A'}
+                rows={'1'}
+                onChange={(e)=>handleChoice(e, key, 'choiceA')}
+              />
+            </div>
+            <div className='flex gap-x-3 items-center'>
+              <RadioButton
+                value={2}
+                onChange={(e)=>handleAnswer(e, key)}
+                name={key}
+              />
+              <TextArea
+                placeholder={'Choice B'}
+                rows={'1'}
+                onChange={(e)=>handleChoice(e, key, 'choiceB')}
+              />
+            </div>
+            <div className='flex gap-x-3 items-center'>
+              <RadioButton
+                value={3}
+                onChange={(e)=>handleAnswer(e, key)}
+                name={key}
+              />
+              <TextArea
+                placeholder={'Choice 3'}
+                rows={'1'}
+                onChange={(e)=>handleChoice(e, key, 'choiceC')}
+              />
+            </div>
+            <div className='flex gap-x-3 items-center'>
+              <RadioButton
+                value={4}
+                onChange={(e)=>handleAnswer(e, key)}
+                name={key}
+              />
+              <TextArea
+                placeholder={'Choice 4'}
+                rows={'1'}
+                onChange={(e)=>handleChoice(e, key, 'choiceD')}
+              />
+            </div>
+          </div>,
+        )}
+      </div>
+    </div>
   );
 };
 
