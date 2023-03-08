911ea7a268aa895ed198a3f9c8a736f74baccaef
MarionRosete <marionrosete1@gmail.com>
2023-03-08 22:41:21 +0800

    Reusable icon button component added

---
+++ src/component/elements/IconButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const IconButton = ({Icon, onClick}) => {
+  return (
+    <div onClick={onClick} className='
+    w-7
+    h-7
+    border-solid
+    border-2
+    border-zinc-600
+    rounded-full
+    flex
+    justify-center
+    items-center
+    cursor-pointer
+    hover:bg-zinc-600
+    hover:border-zinc-600
+  '>
+      <Icon/>
+    </div>
+  );
+};
+IconButton.propTypes = {
+  onClick: PropTypes.func,
+  Icon: PropTypes.any,
+};
+export default IconButton;
